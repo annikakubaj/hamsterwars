8527858496ed2529b27da06e0d00d813948e3458
annika.kubaj <70285472+ak0212@users.noreply.github.com>
2021-04-20 22:11:55 +0200

    GET hamsters to write them out in server

--- routes/hamsters.js
+++ routes/hamsters.js
@@ -6,10 +6,36 @@ const router = express.Router()
 
 
 // REST API 
-router.get('/', (req, res) => {
-	console.log('/hamster REST API');
-	res.send('/hamsters REST API')
+
+// GET /hamsters
+router.get('/', async (req, res) => {
+//	console.log('/hamster REST API');
+//	res.send('/hamsters REST API')
+
+const hamstersRef = db.collection('hamsters')
+const snapshot = await hamstersRef.get()
+
+if ( snapshot.empty) {
+	res.send([])
+	return
+}
+
+let items = []
+snapshot.forEach(doc => {
+	const data = doc.data()
+	data.id = doc.id // id behövs för POST + PUT + DELETE
+	items.push( data )
+
 })
+res.send(items)
+
+
+})
+
+// GET /hamsters/:id
+// POST /hamsters
+// PUT /hamsters/:id
+// DELETE /hamsters/:id
 
 
 
