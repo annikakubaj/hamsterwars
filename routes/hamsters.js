b62bbbff23a9ea360b7223aec99eda28d67d9f0e
annika.kubaj <70285472+ak0212@users.noreply.github.com>
2021-04-21 12:41:04 +0200

    Added GET/hamsters/:id

--- routes/hamsters.js
+++ routes/hamsters.js
@@ -10,7 +10,7 @@ const router = express.Router()
 // GET /hamsters
 router.get('/', async (req, res) => {
 //	console.log('/hamster REST API');
-//	res.send('/hamsters REST API')
+//	res.send('/hamsters REST API') 
 
 const hamstersRef = db.collection('hamsters')
 const snapshot = await hamstersRef.get()
@@ -33,6 +33,19 @@ res.send(items)
 })
 
 // GET /hamsters/:id
+
+router.get('/:id', async (req,res) =>{
+	const id = req.params.id
+	const docRef = await db.collection('hamsters').doc(id).get()
+
+	if( !docRef.exists ) {
+		res.status(404).send('Hamster does not exist')
+
+	}
+
+	const data = docRef.data()
+	res.send(data)
+})
 // POST /hamsters
 // PUT /hamsters/:id
 // DELETE /hamsters/:id
