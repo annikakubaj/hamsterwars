000f42e7aa2b95bd29c253938dde3febed5d573e
annika.kubaj <70285472+ak0212@users.noreply.github.com>
2021-04-27 21:13:57 +0200

    uppdated matchwinner /:id

--- routes/matchwinners.js
+++ routes/matchwinners.js
@@ -5,22 +5,29 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/:id', async (req, res) => {
+try {
 
-
-    const id = req.params.id;
+    const hamsterId = req.params.id;
     const winnerRef = db.collection('matches');
-    const snapshot = await winnerRef.where('winnerId', '==', `${id}`).get();
+    const snapshot = await winnerRef.where('winnerId', '==', `${hamsterId}`).get();
+
+	
     if(snapshot.empty){
-        res.status(404).send(`Hamster with id: ${id} have not won any match yet`);
+        res.status(404).send(`Hamster with id: ${hamsterId} have not won any match yet`);
         return;
     }
     matchwinners = [];
+
     snapshot.forEach(doc =>{
         const data = doc.data();
+		data.id = doc.id
         matchwinners.push(data);
     })
     res.send(matchwinners);
-
+}
+catch(err) {
+	res.status(500).send(err.message)
+}
 })
 
 
